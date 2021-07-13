3ac7f0941914306fb8c48352c93cac80a303d77e
Andre Akira <aah950@hotmail.com>
2021-07-13 14:51:49 -0300

    adicionado atualizar e deletar nos infectados

--- dotnet-mongo-master/Api/Controllers/InfectadoController.cs
+++ dotnet-mongo-master/Api/Controllers/InfectadoController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Data.Collections;
+﻿using System;
+using Api.Data.Collections;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
@@ -34,6 +35,21 @@ namespace Api.Controllers
             var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
             
             return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+            return Ok();
+        }
+
+        [HttpDelete("{dataNasc}")]
+        public ActionResult Delete(DateTime dataNasc)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dataNasc));
+            return Ok();
         }
     }
 }
